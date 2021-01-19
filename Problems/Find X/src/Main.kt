36450567ff045e46ca17914d5c8e86094fc0fbf0
Dunkelweizen <cai.nowicki@gmail.com>
2021-01-19 13:31:19 -0500

    floating-point types

---
+++ Problems/Find X/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a = readLine()!!.toDouble()
+    val b = readLine()!!.toDouble()
+    val c = readLine()!!.toDouble()
+    val aX = c - b
+    val x = aX / a
+    println(x)
+}
